ad71928770aa9985b2b4f1270db7fc98a496efa2
accallejasr <accallejasr@gmail.com>
2025-02-03 13:57:45 -0500

    .
    .

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 import SideBar from "./SideBar";
-import Folder from "./Folder"; 
+import Folder from "./Folder";
 import "../styles/Home.css";
 
 const Home = () => {
   const [selectedFolder, setSelectedFolder] = useState("Recibidos");
+
+  useEffect(() => {
+    setSelectedFolder("Recibidos");
+  }, []);
 
   return (
     <div className="home-container">
       <NavBar />
       <div className="home-content">
         <SideBar onSelectFolder={setSelectedFolder} />
-        <div className="content">
-          <h2>Carpeta seleccionada: {selectedFolder}</h2>
-          <Folder folderName={selectedFolder} /> {/* Mostrar contenido de la carpeta */}
-        </div>
       </div>
     </div>
   );
